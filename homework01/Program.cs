dffe58770a0a8a9d3d9cdd9d8e1d4dddd6447994
Vladimir Korobov <voir703@gmail.com>
2022-11-29 11:27:54 +0500

    add homework 01

---
+++ homework01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+
+System.Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+if (IsPalindrome()) System.Console.WriteLine($" Введёное число - является палиндромом");
+else System.Console.WriteLine($" Число  не является палиндромом");
+System.Console.WriteLine();
